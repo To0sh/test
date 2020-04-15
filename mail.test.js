2bf428fba4337b5ce99c6d5bcbc7ff7f92d67194
Tosh <psychofuck@list.ru>
2020-04-15 18:35:48 +0500

    Added minimal functional for login to mailbox

--- mail.test.js
+++ mail.test.js
@@ -1,7 +1,7 @@
-const { Builder } = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 const url = 'https://mail.yandex.ru/',
-    email = '',
-    password = '',
+    email = 'sergey@nabiullin.com',
+    password = 'sorrowkill',
     message = 'Test message';
 
 
@@ -11,4 +11,8 @@ const url = 'https://mail.yandex.ru/',
         .build();
 
     await driver.get(url);
+    await driver.findElement(By.css('.HeadBanner-Button-Enter')).click();
+    await driver.findElement(By.name('login')).sendKeys(email, Key.ENTER);
+    await driver.wait(until.elementLocated(By.name('passwd')), 5000);
+    await driver.findElement(By.name('passwd')).sendKeys(password, Key.ENTER);
 })();
