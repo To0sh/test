fbb227cb95ac18f87fa7121940e411dc7e6a3b8a
Tosh <psychofuck@list.ru>
2020-04-15 20:37:47 +0500

    Ended functional of sending message

--- mail.test.js
+++ mail.test.js
@@ -3,7 +3,7 @@ const  { Builder, By, Key, until } = require('selenium-webdriver');
 
 let chromeCapabilities = webdriver.Capabilities.chrome();
 let chromeOptions = {
-    'args': ['--disable-notifications']
+    'args': ['--disable-notifications', '--start-maximized']
 };
 
 chromeCapabilities.set('chromeOptions', chromeOptions);
@@ -12,6 +12,7 @@ const url = 'https://mail.yandex.ru/',
       seleniumHost = 'http://localhost:4444/wd/hub',
       email = 'sergey@nabiullin.com',
       password = 'sorrowkill',
+      subject = 'Test',
       message = 'Test message';
 
 
@@ -25,11 +26,19 @@ let driver =  new Builder()
     try {
         await driver.get(url);
 
-        await driver.findElement(By.css('.HeadBanner-Button-Enter')).click();
+        await driver.findElement(By.className('HeadBanner-Button-Enter')).click();
         await driver.findElement(By.name('login')).sendKeys(email, Key.ENTER);
         await driver.wait(until.elementLocated(By.name('passwd')), 5000);
         await driver.findElement(By.name('passwd')).sendKeys(password, Key.ENTER);
 
+        await driver.wait(until.elementLocated(By.className('mail-ComposeButton')), 5000);
+        await driver.findElement(By.className('mail-ComposeButton')).click();
+
+        await driver.wait(until.elementLocated(By.css('.composeYabbles')), 5000);
+        await driver.findElement(By.className('composeYabbles')).sendKeys(email);
+        await driver.findElement(By.className('ComposeSubject-TextField')).sendKeys(subject);
+        await driver.findElement(By.className('cke_wysiwyg_div ')).sendKeys(message);
+        await driver.findElement(By.className('ComposeControlPanelButton-Button_action')).click();
 
     } catch (e) {
         console.log(e.message);
